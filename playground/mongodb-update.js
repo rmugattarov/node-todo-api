f6204a5ef8389fff1776e2c730d528b434b52663
Robert Mugattarov <mugattarov@gmail.com>
2018-11-23 13:28:38 +0300

    Updates

---
+++ playground/mongodb-update.js
@@ -0,0 +1,20 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  db.collection('Todos')
+  .findOneAndUpdate(
+    {a: 'a'},
+    {$set: {a: 'b'}},
+    {returnOriginal: false}
+  ).then((res) => {
+    console.log(res);
+  });
+
+  client.close();
+});
