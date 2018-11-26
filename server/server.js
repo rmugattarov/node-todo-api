e1ecb20a0013d5e50527b14315c0b5f1396d8e9f
Robert Mugattarov <mugattarov@gmail.com>
2018-11-26 13:22:49 +0300

    Updates

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {ObjectID} = require('mongodb');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
 
 var app = express();
 const port = process.env.PORT || 3000;
