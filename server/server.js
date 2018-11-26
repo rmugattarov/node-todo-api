4852a5be8090277e3a4f5ef5184f45c98fb62c5b
Robert Mugattarov <mugattarov@gmail.com>
2018-11-26 11:39:41 +0300

    Updates

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+var {ObjectId} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)) {
+    res.status(404).send('id not valid');
+  } else {
+      Todo.findById(id).then((todo) => {
+        if(todo) {
+          res.send(todo);
+        } else {
+          res.status(404).send('todo not found');
+        }
+      }, (e) => {
+        res.send(e);
+      });
+  }
 });
 
 app.listen(3000, () => {
