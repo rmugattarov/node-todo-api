018ba95b2a7c3685fb449347310e719876148c05
Robert Mugattarov <mugattarov@gmail.com>
2018-11-26 11:49:00 +0300

    Updates

--- server/server.js
+++ server/server.js
@@ -35,18 +35,18 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if(!ObjectId.isValid(id)) {
-    res.status(404).send('id not valid');
-  } else {
-      Todo.findById(id).then((todo) => {
-        if(todo) {
-          res.send(todo);
-        } else {
-          res.status(404).send('todo not found');
-        }
-      }, (e) => {
-        res.send(e);
-      });
+    return res.status(404).send('id not valid');
   }
+  Todo.findById(id).then((todo) => {
+    if(todo) {
+      res.send({todo});
+    } else {
+      res.status(404).send('todo not found');
+    }
+  }, (e) => {
+    res.status(404).send();
+  });
+
 });
 
 app.listen(3000, () => {
