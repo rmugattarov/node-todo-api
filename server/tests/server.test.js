d14de6539b2c628f65c0bee3416cb9eefe8ffc5c
Robert Mugattarov <mugattarov@gmail.com>
2018-11-28 12:06:34 +0300

    Updates

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -15,87 +15,87 @@ describe('POST /todos', () => {
     var text = 'test string';
 
     request(app)
-      .post('/todos')
-      .send({text})
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.text)
-          .toBe(text);
-      })
-      .end((err, res) => {
-        if(err) {
-          return done(err);
-        }
-        Todo.find({text}).then((todos) => {
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
-          done();
-        }).catch((e) => done(e));
-      });
+    .post('/todos')
+    .send({text})
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.text)
+      .toBe(text);
+    })
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
+      Todo.find({text}).then((todos) => {
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch((e) => done(e));
+    });
   });
 
   it('should not create todo with invalid data', (done) => {
     request(app)
-      .post('/todos')
-      .send(' ')
-      .expect(400)
-      .end((err, res) => {
-        if(err) {
-          return done(err);
-        }
-        Todo.find().then((todos) => {
-          expect(todos.length).toBe(2);
-          done();
-        }).catch((e) => done(e));
-      });
+    .post('/todos')
+    .send(' ')
+    .expect(400)
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(2);
+        done();
+      }).catch((e) => done(e));
+    });
   });
 });
 
 describe('GET /todos', () => {
   it('should return all todos', (done) => {
     request(app)
-      .get('/todos')
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.todos.length).toBe(2);
-      })
-      .end(done);
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
   })
 });
 
 describe('GET /todos/:id', () => {
   it('should return todo for valid id', (done) => {
     request(app)
-      .get(`/todos/${todos[0]._id.toHexString()}`)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.todo.text).toBe(todos[0].text);
-      })
-      .end(done);
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
   });
 });
 
 describe('GET /users/me', () => {
   it('should return user if authencticated', (done) => {
     request(app)
-      .get('/users/me')
-      .set('x-auth', users[0].tokens[0].token)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body._id).toBe(users[0]._id.toHexString());
-        expect(res.body.email).toBe(users[0].email);
-      })
-      .end(done);
+    .get('/users/me')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body._id).toBe(users[0]._id.toHexString());
+      expect(res.body.email).toBe(users[0].email);
+    })
+    .end(done);
   });
 
   it('should return 401 if not authenticated', (done) =>{
     request(app)
-      .get('/users/me')
-      .expect(401)
-      .expect((res) => {
-        expect(res.body).toEqual({});
-      })
-      .end(done);
+    .get('/users/me')
+    .expect(401)
+    .expect((res) => {
+      expect(res.body).toEqual({});
+    })
+    .end(done);
   })
 });
 
@@ -105,43 +105,89 @@ describe('POST /users', () => {
     var password = '123456';
 
     request(app)
-      .post('/users')
-      .send({email, password})
-      .expect(200)
-      .expect((res) => {
-        expect(res.headers['x-auth']).toBeTruthy();
-        expect(res.body).toBeTruthy();
-        expect(res.body.email).toBe(email);
-      })
-      .end((err) => {
-        if(err){
-          return done(err);
-        }
-        User.findOne({email}).then((user) => {
-          expect(user).toBeTruthy();
-          expect(user.password).not.toEqual(password);
-          done();
-        });
-      });
+    .post('/users')
+    .send({email, password})
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toBeTruthy();
+      expect(res.body).toBeTruthy();
+      expect(res.body.email).toBe(email);
+    })
+    .end((err) => {
+      if(err){
+        return done(err);
+      }
+      User.findOne({email}).then((user) => {
+        expect(user).toBeTruthy();
+        expect(user.password).not.toEqual(password);
+        done();
+      }).catch((err) => done(err));
+    });
   });
   it('should return validation errors if request invalid', (done) => {
     var email = 'bademail';
     var password;
 
     request(app)
-      .post('/users')
-      .send({email, password})
-      .expect(400)
-      .end(done);
+    .post('/users')
+    .send({email, password})
+    .expect(400)
+    .end(done);
   });
   it('should not create user if email in use', (done) => {
     var email = '111@111.com';
     var password = '123456';
 
     request(app)
-      .post('/users')
-      .send({email, password})
-      .expect(400)
-      .end(done);
+    .post('/users')
+    .send({email, password})
+    .expect(400)
+    .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toBeTruthy();
+    })
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens[0]).toMatchObject({
+          access: 'auth',
+          token: res.headers['x-auth']
+        });
+        done();
+      }).catch((err) => done(err));
+    });
+  });
+
+  it('should login user and return auth token', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: 'badpassword'
+    })
+    .expect(400)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toBeFalsy();
+    })
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
+      done();
+    });
   });
 });
